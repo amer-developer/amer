c4899cac91c565efb883fcec86e22008ebe5999f
Saud Alhelali <alhilali@hotmail.com>
2021-04-09 15:05:16 +0300

    make sure optional in gql

--- src/common/dto/PageOptionsDto.ts
+++ src/common/dto/PageOptionsDto.ts
@@ -29,7 +29,7 @@ export class PageOptionsDto {
     @IsOptional()
     readonly order: Order = Order.ASC;
 
-    @Field()
+    @Field({ nullable: true })
     @ApiPropertyOptional()
     @IsString()
     @IsOptional()
